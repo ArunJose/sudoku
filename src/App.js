61c7cbd33bad0a6808b7393c62bbbf544e1ffff6
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-09-16 09:37:42 +0530

    Add game grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import generatePuzzle from "./helpers/generate-puzzle";
+import "./App.css";
 
 function App() {
+  const [gameBoard, setGameBoard] = useState(generatePuzzle());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Sudoku</h1>
+        <div className="top-bar">
+          {/*
+          TODO:
+          Select difficulty from options
+          Toggle to turn of 'Check for mistakes'
+          */}
+        </div>
+        <div>
+          <div className="game-board">
+            <table>
+              {gameBoard.map((row) => {
+                const tr = row.map((cell) => <td>{cell}</td>);
+                return <tr>{tr}</tr>;
+              })}
+            </table>
+          </div>
+          <div className="button-pane">
+            <button>New game</button>
+            <div className="num-board">
+              {/*
+                TODO:
+                Number buttons to input numbers into game grid
+              */}
+            </div>
+            <div className="control-buttons">
+              <button>Hint</button>
+              <button>Clear</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
